9445a358648e4d2e68f1d928cd7bb3b56dfd0ea6
lorenaguo <lorenaguo22@gmail.com>
2025-02-01 23:39:20 -0500

    try to add frontend again

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center p-10 bg-blue-500 text-white">
+      <h1 className="text-4xl">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+export default App;
